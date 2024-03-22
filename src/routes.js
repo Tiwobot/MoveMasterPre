56aea206b545568be112936ca2699107ccacc3f3
tiwobot <twogun.pre@gmail.com>
2024-03-23 01:11:19 +0300

    comment out RTL function

--- src/routes.js
+++ src/routes.js
@@ -25,7 +25,7 @@
 import Dashboard from "layouts/dashboard";
 import Tables from "layouts/tables";
 import Billing from "layouts/billing";
-import RTL from "layouts/rtl";
+//import RTL from "layouts/rtl";
 import Notifications from "layouts/notifications";
 import Profile from "layouts/profile";
 import SignIn from "layouts/authentication/sign-in";
@@ -59,14 +59,14 @@ const routes = [
     route: "/billing",
     component: <Billing />,
   },
-  {
+ /* {
     type: "collapse",
     name: "RTL",
     key: "rtl",
     icon: <Icon fontSize="small">format_textdirection_r_to_l</Icon>,
     route: "/rtl",
     component: <RTL />,
-  },
+  }, */
   {
     type: "collapse",
     name: "Notifications",
